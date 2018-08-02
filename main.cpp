898a09213d005eea3fb1b655eb3ffd1e37d21986
vezviitiko <38458279+vezviitiko@users.noreply.github.com>
2018-08-02 15:18:43 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "TestGui.h"
+
+TestGui::TestGui()
+{
+	dlg(tab1, "Main")(tab2, "Maps")(tab3, "Time")
+	   .Title("Kamaz-Migo Interface")
+	   .AddFrame(menu);
+	menu.Set([=](Bar& bar) {
+		bar.Sub("Menu", [=](Bar& bar) { SubMenu(bar); });
+	});
+	
+	// -- лист 1 - tab1
+	Tab1Screen();
+	// -- лист 2 - tab2
+	Tab2Screen();
+	
+    dlg.Run();
+}
+
+GUI_APP_MAIN
+{
+	LOG("START");
+	TestGui();
+	LOG("END");
+}
