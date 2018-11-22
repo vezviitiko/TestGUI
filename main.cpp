4422749ecdc4604624865c63833ec4ad9b703d97
vezviitiko <38458279+vezviitiko@users.noreply.github.com>
2018-11-22 17:03:40 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,23 +2,31 @@
 
 TestGui::TestGui()
 {
+	dlg.Sizeable().Zoomable().Icon(TutorialImg::logo_iaz());
+	//dlg.MaximizeBox();
+	//dlg.Maximize();
 	
 	Pointf p(37.8090947, 55.9121311);
-	//(MapDlg(p), "Карта")
 	tabMap.Set(p);
-	dlg(tabMain, "Главная")(tabMap, "Карта")(tabPath, "Пути")(tabMan, "Руководство");
 	
+	dlg(tabMain, "Главная")(tabCharts, "Графики")(tabMap, "Карта")(tabPath, "Пути")(tabMan, "Руководство").Title("Kamaz-SolNavPro Interface");
+	
+	// если потребуется меню сверху
 	//dlg(tab1, "OLD_Main")(tab2, "Maps")(tab3, "Path")(tabMain, "Main")
 	/*dlg(tabMain, "Main")
 	   .Title("Kamaz-Migo Interface")
-	   .AddFrame(menu);
+	   .AddFrame(menu);*/
+	/*
 	menu.Set([=](Bar& bar) {
 		bar.Sub("Menu", [=](Bar& bar) { SubMenu(bar); });
 	});*/
+	
 	TabMainScreen();
 	TabPathScreen();
 	TabManScreen();
-    dlg.Run();
+	TabChartsScreen();
+	
+	dlg.Run();
 }
 
 GUI_APP_MAIN
