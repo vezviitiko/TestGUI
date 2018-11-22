4288f082a19758e151c1b20c53d8b381b045e9c2
vezviitiko <38458279+vezviitiko@users.noreply.github.com>
2018-11-22 17:03:56 +0300

    Update TestGui.h

--- TestGui.h
+++ TestGui.h
@@ -2,6 +2,9 @@
 #define _TestGui_TestGui_h
 
 #include <CtrlLib/CtrlLib.h>
+#include <ScatterCtrl/ScatterCtrl.h>	// библиотека графиков
+#include <kamaz/kamaz.h>				// библиотека перевода значений
+#include <MishenCtrl/MishenCtrl.h>		// библиотека мишени
 
 using namespace Upp;
 
@@ -12,30 +15,72 @@ using namespace Upp;
 #define LAYOUTFILE <TestGui/TabDlg.lay>
 #include <CtrlCore/lay.h>
 
+// Библиотека Карты
 #include "MapDlg.h"
 
 class TestGui : public TopWindow {
 public:
 	typedef TestGui CLASSNAME;
 	TestGui();
-	MenuBar menu;
+	//MenuBar menu;
 	TabDlg dlg;
 	bool butstop;
-	int colpoint;
+	int colpoint = 1; // для подсчета количества отображений
+	// ------------ tabMain
 	WithTabMainLayout<ParentCtrl> tabMain;
 	void TabMainScreen();
 	void ButStart();
 	void ButStop();
 	void ZeroTarget();
 	void ButZeropop();
+	void ShowCoordinates(bool show = true) {showCoordinates = show;}
+	void ShowCoordSwitch();
+	
+	// кнопки для графиков
+	void PgDownsc1();
+	void PgUpsc1();
+	void Plussc1();
+	void Minussc1();
+	void PgDownsc2();
+	void PgUpsc2();
+	void Plussc2();
+	void Minussc2();
+	void PgBack();
+	void PgForward();
+	
+	// ------------ tabPath
 	WithTabPathLayout<ParentCtrl> tabPath;
 	void TabPathScreen();
 	void OverWritePathIni();
 	
+	// ------------ tabMap
 	MapDlgDlg tabMap;
 	
+	// ------------ tabCharts
+	WithTabChartsLayout<ParentCtrl> tabCharts;
+	void TabChartsScreen();
+	void PgChDown();
+	void PgChUp();
+	void PgChBack();
+	void PgChForward();
+	void PgChPlus();
+	void PgChMinus();
+	void PgChPlusX();
+	void PgChMinusX();
+	void PgChPlusY();
+	void PgChMinusY();
+	void ChOnSeries();
+	void ChAnimation(double edx,double edy,double edz,double eddx,double eddy, double eddz, double dlat, double dlon, double dH, double dSD, int N, double t);
+	
+	// ------------ tabMan
 	WithTabManLayout<ParentCtrl> tabMan;
 	void TabManScreen();
+	
+private:
+	// ------------ tabMain and tabCharts
+	double N, t;
+	Vector<Pointf> x, y, z, dx, dy, dz, lat, lon, H, SD;
+	bool showCoordinates;
 };
 
 struct PathIni
